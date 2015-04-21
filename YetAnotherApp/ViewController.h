ae9716390578ac6fd4b5bda0037f03ead2f6d408
James Dunwoody <jdunwoody@odecee.com.au>
2015-04-21 14:44:35 +1000

    Configured git, ruby, bundler

---
+++ YetAnotherApp/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  YetAnotherApp
+//
+//  Created by James Dunwoody on 21/04/15.
+//  Copyright (c) 2015 Odecee. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ YetAnotherApp/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  YetAnotherApp
+//
+//  Created by James Dunwoody on 21/04/15.
+//  Copyright (c) 2015 Odecee. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
